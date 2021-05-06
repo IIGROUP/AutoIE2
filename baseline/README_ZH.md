451729fc0c72e7964630261f72fcfe3e098b1288
Chestnut1999 <44120124+Chestnut1999@users.noreply.github.com>
2021-05-07 00:03:01 +0800

    Add files via upload

---
+++ baseline/README_ZH.md
@@ -0,0 +1,74 @@
+[**English**](https://github.com/IIGROUP/AutoIE2/blob/main/baseline/README.md) | [**中文**](https://github.com/IIGROUP/AutoIE2/blob/main/baseline/README_ZH.md)
+
+本目录是NLPCC-2021 Shared Task on AutoIE2: Sub-Event Identification任务的baseline。解决方案基于“UER: An Open-Source Toolkit for Pre-training Models”工作。在使用时请进行引用。
+
+## 依赖环境
+* Python >= 3.6
+* torch >= 1.1
+* six >= 1.12.0
+* argparse
+* packaging
+* 如果使用混合精度，需要安装英伟达的apex
+* 如果涉及到TensorFlow模型的转换，需要安装TensorFlow
+* 如果在tokenizer中使用sentencepiece模型，需要安装[SentencePiece](https://github.com/google/sentencepiece)
+* 如果使用模型集成stacking，需要LightGBM和[BayesianOptimization](https://github.com/fmfn/BayesianOptimization)
+* 如果使用全词遮罩（whole word masking）预训练，需要分词工具，例如[jieba](https://github.com/fxsjy/jieba)
+
+## 数据
+* 将AutoIE2目录的taskdata路径下所有文件复制到此目录下的data文件夹中。
+* 数据包括“COVID-19_train.tsv”，“Tokyo-Olympic-Games_train.tsv”，“Trade-War_train.tsv”，“unlabeled-corpus.tsv”。
+
+## 快速上手
+* 下载Google中文预训练模型[*google_zh_model.bin*](https://share.weiyun.com/A1C49VPb)，并将其放在 *models* 文件夹中。接着加载Google中文预训练模型。预训练模型由词向量层，编码层和目标任务层组成。
+* 在下游分类数据集上微调预训练模型 *google_zh_model.bin*:
+```
+python3 run_classifier.py --pretrained_model_path models/google_zh_model.bin --vocab_path models/google_zh_vocab.txt \
+                          --train_path [train_path] --dev_path [dev_path] --test_path [test_path] \
+                          --epochs_num 3 --batch_size 32 --embedding word_pos_seg --encoder transformer --mask fully_visible
+```
+* 微调后的模型的默认路径是*models/finetuned_model.bin*, 然后利用微调后的分类器模型进行预测：
+```
+python3 inference/run_classifier_infer.py --load_model_path models/finetuned_model.bin --vocab_path models/google_zh_vocab.txt \
+                                          --test_path [test_path] \
+                                          --prediction_path data/prediction.tsv --labels_num 2 \
+                                          --embedding word_pos_seg --encoder transformer --mask fully_visible
+```
+*--test_path* 指定需要预测的文件；<br>
+*--prediction_path* 指定预测结果的文件；<br>
+注意需要指定分类任务标签的个数 *--labels_num* ，这里是二分类任务。
+注意在微调阶段使用 *--output_model_path* 指定微调后的模型的输出路径。预训练的实际batch size大小是 *--batch_size* 乘以 *--world_size*；分类的实际的batch size大小是 *--batch_size* 。
+
+## 使用说明
+项目组织如下：
+```
+UER-py/
+    |--uer/
+    |    |--encoders/ # 包括编码器模块，例如RNN, CNN, Transformer
+    |    |--layers/ # 包括常用的神经网络层
+    |    |--models/ # 包括 model.py，用于组合词向量（embedding）、编码器（encoder）、目标任务（target）模块
+    |    |--utils/ # 包括常用的功能模块
+    |    |--model_loader.py
+    |    |--model_saver.py
+    |    |--opts.py
+    |
+    |--datasets/ # 下游任务数据集存放文件夹
+    |--models/ # 模型、词典、配置文件存放文件夹
+    |--inference/ # 前向推理脚本存放文件夹
+    |
+    |--run_classifier.py # 分类脚本
+    |--README.md
+    |--README_ZH.md
+
+```
+
+## 引用
+#### 如果您在您的学术工作中使用我们的工作（比如预训练模型权重），可以引用我们的[论文](https://arxiv.org/pdf/1909.05658.pdf)：
+```
+@article{zhao2019uer,
+  title={UER: An Open-Source Toolkit for Pre-training Models},
+  author={Zhao, Zhe and Chen, Hui and Zhang, Jinbin and Zhao, Xin and Liu, Tao and Lu, Wei and Chen, Xi and Deng, Haotang and Ju, Qi and Du, Xiaoyong},
+  journal={EMNLP-IJCNLP 2019},
+  pages={241},
+  year={2019}
+}
+```
---
+++ baseline/run_classifier.py
@@ -0,0 +1,339 @@
+"""
+This script provides an exmaple to wrap UER-py for classification.
+"""
+import random
+import argparse
+import torch
+import torch.nn as nn
+from uer.layers import *
+from uer.encoders import *
+from uer.utils.vocab import Vocab
+from uer.utils.constants import *
+from uer.utils import *
+from uer.utils.optimizers import *
+from uer.utils.config import load_hyperparam
+from uer.utils.seed import set_seed
+from uer.model_saver import save_model
+from uer.opts import finetune_opts
+
+
+class Classifier(nn.Module):
+    def __init__(self, args):
+        super(Classifier, self).__init__()
+        self.embedding = str2embedding[args.embedding](args, len(args.tokenizer.vocab))
+        self.encoder = str2encoder[args.encoder](args)
+        self.labels_num = args.labels_num
+        self.pooling = args.pooling
+        self.soft_targets = args.soft_targets
+        self.soft_alpha = args.soft_alpha
+        self.output_layer_1 = nn.Linear(args.hidden_size, args.hidden_size)
+        self.output_layer_2 = nn.Linear(args.hidden_size, self.labels_num)
+
+    def forward(self, src, tgt, seg, soft_tgt=None):
+        """
+        Args:
+            src: [batch_size x seq_length]
+            tgt: [batch_size]
+            seg: [batch_size x seq_length]
+        """
+        # Embedding.
+        emb = self.embedding(src, seg)
+        # Encoder.
+        output = self.encoder(emb, seg)
+        # Target.
+        if self.pooling == "mean":
+            output = torch.mean(output, dim=1)
+        elif self.pooling == "max":
+            output = torch.max(output, dim=1)[0]
+        elif self.pooling == "last":
+            output = output[:, -1, :]
+        else:
+            output = output[:, 0, :]
+        output = torch.tanh(self.output_layer_1(output))
+        logits = self.output_layer_2(output)
+        if tgt is not None:
+            if self.soft_targets and soft_tgt is not None:
+                loss = self.soft_alpha * nn.MSELoss()(logits, soft_tgt) + \
+                       (1 - self.soft_alpha) * nn.NLLLoss()(nn.LogSoftmax(dim=-1)(logits), tgt.view(-1))
+            else:
+                loss = nn.NLLLoss()(nn.LogSoftmax(dim=-1)(logits), tgt.view(-1))
+            return loss, logits
+        else:
+            return None, logits
+
+
+def count_labels_num(path):
+    labels_set, columns = set(), {}
+    with open(path, mode="r", encoding="utf-8") as f:
+        for line_id, line in enumerate(f):
+            if line_id == 0:
+                for i, column_name in enumerate(line.strip().split("\t")):
+                    columns[column_name] = i
+                continue
+            line = line.strip().split("\t")
+            label = int(line[columns["label"]])
+            labels_set.add(label)
+    return len(labels_set)
+
+
+def load_or_initialize_parameters(args, model):
+    if args.pretrained_model_path is not None:
+        # Initialize with pretrained model.
+        model.load_state_dict(torch.load(args.pretrained_model_path), strict=False)
+    else:
+        # Initialize with normal distribution.
+        for n, p in list(model.named_parameters()):
+            if "gamma" not in n and "beta" not in n:
+                p.data.normal_(0, 0.02)
+
+
+def build_optimizer(args, model):
+    param_optimizer = list(model.named_parameters())
+    no_decay = ['bias', 'gamma', 'beta']
+    optimizer_grouped_parameters = [
+                {'params': [p for n, p in param_optimizer if not any(nd in n for nd in no_decay)], 'weight_decay_rate': 0.01},
+                {'params': [p for n, p in param_optimizer if any(nd in n for nd in no_decay)], 'weight_decay_rate': 0.0}
+    ]
+    if args.optimizer in ["adamw"]:
+        optimizer = str2optimizer[args.optimizer](optimizer_grouped_parameters, lr=args.learning_rate, correct_bias=False)
+    else:
+        optimizer = str2optimizer[args.optimizer](optimizer_grouped_parameters, lr=args.learning_rate,
+                                                  scale_parameter=False, relative_step=False)
+    if args.scheduler in ["constant"]:
+        scheduler = str2scheduler[args.scheduler](optimizer)
+    elif args.scheduler in ["constant_with_warmup"]:
+        scheduler = str2scheduler[args.scheduler](optimizer, args.train_steps*args.warmup)
+    else:
+        scheduler = str2scheduler[args.scheduler](optimizer, args.train_steps*args.warmup, args.train_steps)
+    return optimizer, scheduler
+
+
+def batch_loader(batch_size, src, tgt, seg, soft_tgt=None):
+    instances_num = src.size()[0]
+    for i in range(instances_num // batch_size):
+        src_batch = src[i * batch_size : (i + 1) * batch_size, :]
+        tgt_batch = tgt[i * batch_size : (i + 1) * batch_size]
+        seg_batch = seg[i * batch_size : (i + 1) * batch_size, :]
+        if soft_tgt is not None:
+            soft_tgt_batch = soft_tgt[i * batch_size : (i + 1) * batch_size, :]
+            yield src_batch, tgt_batch, seg_batch, soft_tgt_batch
+        else:
+            yield src_batch, tgt_batch, seg_batch, None
+    if instances_num > instances_num // batch_size * batch_size:
+        src_batch = src[instances_num // batch_size * batch_size :, :]
+        tgt_batch = tgt[instances_num // batch_size * batch_size :]
+        seg_batch = seg[instances_num // batch_size * batch_size :, :]
+        if soft_tgt is not None:
+            soft_tgt_batch = soft_tgt[instances_num // batch_size * batch_size :, :]
+            yield src_batch, tgt_batch, seg_batch, soft_tgt_batch
+        else:
+            yield src_batch, tgt_batch, seg_batch, None
+
+
+def read_dataset(args, path):
+    dataset, columns = [], {}
+    with open(path, mode="r", encoding="utf-8") as f:
+        for line_id, line in enumerate(f):
+            if line_id == 0:
+                for i, column_name in enumerate(line.strip().split("\t")):
+                    columns[column_name] = i
+                continue
+            line = line[:-1].split("\t")
+            tgt = int(line[columns["label"]])
+            if args.soft_targets and "logits" in columns.keys():
+                soft_tgt = [float(value) for value in line[columns["logits"]].split(" ")]
+            if "text_b" not in columns:  # Sentence classification.
+                text_a = line[columns["text_a"]]
+                src = args.tokenizer.convert_tokens_to_ids([CLS_TOKEN] + args.tokenizer.tokenize(text_a))
+                seg = [1] * len(src)
+            else:  # Sentence-pair classification.
+                text_a, text_b = line[columns["text_a"]], line[columns["text_b"]]
+                src_a = args.tokenizer.convert_tokens_to_ids([CLS_TOKEN] + args.tokenizer.tokenize(text_a) + [SEP_TOKEN])
+                src_b = args.tokenizer.convert_tokens_to_ids(args.tokenizer.tokenize(text_b) + [SEP_TOKEN])
+                src = src_a + src_b
+                seg = [1] * len(src_a) + [2] * len(src_b)
+
+            if len(src) > args.seq_length:
+                src = src[: args.seq_length]
+                seg = seg[: args.seq_length]
+            while len(src) < args.seq_length:
+                src.append(0)
+                seg.append(0)
+            if args.soft_targets and "logits" in columns.keys():
+                dataset.append((src, tgt, seg, soft_tgt))
+            else:
+                dataset.append((src, tgt, seg))
+
+    return dataset
+
+
+def train_model(args, model, optimizer, scheduler, src_batch, tgt_batch, seg_batch, soft_tgt_batch=None):
+    model.zero_grad()
+
+    src_batch = src_batch.to(args.device)
+    tgt_batch = tgt_batch.to(args.device)
+    seg_batch = seg_batch.to(args.device)
+    if soft_tgt_batch is not None:
+        soft_tgt_batch = soft_tgt_batch.to(args.device)
+
+    loss, _ = model(src_batch, tgt_batch, seg_batch, soft_tgt_batch)
+    if torch.cuda.device_count() > 1:
+        loss = torch.mean(loss)
+
+    if args.fp16:
+        with args.amp.scale_loss(loss, optimizer) as scaled_loss:
+            scaled_loss.backward()
+    else:
+        loss.backward()
+
+    optimizer.step()
+    scheduler.step()
+
+    return loss
+
+
+def evaluate(args, dataset, print_confusion_matrix=False):
+    src = torch.LongTensor([sample[0] for sample in dataset])
+    tgt = torch.LongTensor([sample[1] for sample in dataset])
+    seg = torch.LongTensor([sample[2] for sample in dataset])
+
+    batch_size = args.batch_size
+
+    correct = 0
+    # Confusion matrix.
+    confusion = torch.zeros(args.labels_num, args.labels_num, dtype=torch.long)
+
+    args.model.eval()
+
+    for i, (src_batch, tgt_batch, seg_batch, _) in enumerate(batch_loader(batch_size, src, tgt, seg)):
+        src_batch = src_batch.to(args.device)
+        tgt_batch = tgt_batch.to(args.device)
+        seg_batch = seg_batch.to(args.device)
+        with torch.no_grad():
+            _, logits = args.model(src_batch, tgt_batch, seg_batch)
+        pred = torch.argmax(nn.Softmax(dim=1)(logits), dim=1)
+        gold = tgt_batch
+        for j in range(pred.size()[0]):
+            confusion[pred[j], gold[j]] += 1
+        correct += torch.sum(pred == gold).item()
+
+    if print_confusion_matrix:
+        print("Confusion matrix:")
+        print(confusion)
+        print("Report precision, recall, and f1:")
+        for i in range(confusion.size()[0]):
+            p = confusion[i, i].item() / confusion[i, :].sum().item()
+            r = confusion[i, i].item() / confusion[:, i].sum().item()
+            f1 = 2 * p * r / (p + r)
+            print("Label {}: {:.3f}, {:.3f}, {:.3f}".format(i, p, r, f1))
+
+    print("Acc. (Correct/Total): {:.4f} ({}/{}) ".format(correct / len(dataset), correct, len(dataset)))
+    return correct / len(dataset), confusion
+
+
+def main():
+    parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+
+    finetune_opts(parser)
+
+    parser.add_argument("--pooling", choices=["mean", "max", "first", "last"], default="first",
+                        help="Pooling type.")
+
+    parser.add_argument("--tokenizer", choices=["bert", "char", "space"], default="bert",
+                        help="Specify the tokenizer."
+                             "Original Google BERT uses bert tokenizer on Chinese corpus."
+                             "Char tokenizer segments sentences into characters."
+                             "Space tokenizer segments sentences into words according to space."
+                             )
+
+    parser.add_argument("--soft_targets", action='store_true',
+                        help="Train model with logits.")
+    parser.add_argument("--soft_alpha", type=float, default=0.5,
+                        help="Weight of the soft targets loss.")
+
+    args = parser.parse_args()
+
+    # Load the hyperparameters from the config file.
+    args = load_hyperparam(args)
+
+    set_seed(args.seed)
+
+    # Count the number of labels.
+    args.labels_num = count_labels_num(args.train_path)
+
+    # Build tokenizer.
+    args.tokenizer = str2tokenizer[args.tokenizer](args)
+
+    # Build classification model.
+    model = Classifier(args)
+
+    # Load or initialize parameters.
+    load_or_initialize_parameters(args, model)
+
+    args.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    model = model.to(args.device)
+
+    # Training phase.
+    trainset = read_dataset(args, args.train_path)
+    random.shuffle(trainset)
+    instances_num = len(trainset)
+    batch_size = args.batch_size
+
+    src = torch.LongTensor([example[0] for example in trainset])
+    tgt = torch.LongTensor([example[1] for example in trainset])
+    seg = torch.LongTensor([example[2] for example in trainset])
+    if args.soft_targets:
+        soft_tgt = torch.FloatTensor([example[3] for example in trainset])
+    else:
+        soft_tgt = None
+
+    args.train_steps = int(instances_num * args.epochs_num / batch_size) + 1
+
+    print("Batch size: ", batch_size)
+    print("The number of training instances:", instances_num)
+
+    optimizer, scheduler = build_optimizer(args, model)
+
+    if args.fp16:
+        try:
+            from apex import amp
+        except ImportError:
+            raise ImportError("Please install apex from https://www.github.com/nvidia/apex to use fp16 training.")
+        model, optimizer = amp.initialize(model, optimizer, opt_level=args.fp16_opt_level)
+        args.amp = amp
+
+    if torch.cuda.device_count() > 1:
+        print("{} GPUs are available. Let's use them.".format(torch.cuda.device_count()))
+        model = torch.nn.DataParallel(model)
+    args.model = model
+
+    total_loss, result, best_result = 0.0, 0.0, 0.0
+
+    print("Start training.")
+
+    for epoch in range(1, args.epochs_num + 1):
+        model.train()
+        for i, (src_batch, tgt_batch, seg_batch, soft_tgt_batch) in enumerate(batch_loader(batch_size, src, tgt, seg, soft_tgt)):
+            loss = train_model(args, model, optimizer, scheduler, src_batch, tgt_batch, seg_batch, soft_tgt_batch)
+            total_loss += loss.item()
+            if (i + 1) % args.report_steps == 0:
+                print("Epoch id: {}, Training steps: {}, Avg loss: {:.3f}".format(epoch, i + 1, total_loss / args.report_steps))
+                total_loss = 0.0
+
+        result = evaluate(args, read_dataset(args, args.dev_path))
+        if result[0] > best_result:
+            best_result = result[0]
+            save_model(model, args.output_model_path)
+
+    # Evaluation phase.
+    if args.test_path is not None:
+        print("Test set evaluation.")
+        if torch.cuda.device_count() > 1:
+            model.module.load_state_dict(torch.load(args.output_model_path))
+        else:
+            model.load_state_dict(torch.load(args.output_model_path))
+        evaluate(args, read_dataset(args, args.test_path), True)
+
+
+if __name__ == "__main__":
+    main()
